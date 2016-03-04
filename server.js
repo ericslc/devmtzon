4a98a529dd9c380423db9b61244a2c9b2eccda12
ericslc <eric.leatham@gmail.com>
2016-03-03 17:09:09 -0700

    updated again

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 // TODO: import express and initialize the app
 
-app.use(express.static(__dirname + '/public'));
-var express = require('express'),
-		bodyParser = require('body-parser'),
-    app = express(),
-    port = 8005;
 
-		app.use(express.static(__dirname + '/public'));
-		app.use(bodyParser.urlencoded({extended: true}));
-		app.use(bodyParser.json());
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var port = 8005;
 
-		app.get('/api/getProduct/:id', function(req, res) {
+
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/api/getProductList', function(req, res) {
+		 res.send(products)
+
+		});
+
+app.get('/api/getProduct/:id', function(req, res) {
     var product = _.findIndex(products, {_id: req.params.id});
     var item = products.slice(product, product + 1);
     res.json(item)
@@ -21,8 +27,30 @@ app.post('/api/getProductList', function(req, res) {
     products.push(product);
     res.send(product);
 });
+app.put('/api/getProduct/:id', function(req, res){
+	var update = req.body;
+	productIndex = _.findIndex(products,{_id: req.params.id});
 
+	console.log(update);
+	console.log(productIndex);
 
-app.listen(8005, function () {
-	console.log('Listening on port', 8005);
+	if (!products[productIndex]) {
+		res.send('not found');
+	} else {
+		var updatedProduct = _.assign(productIndex[productIndex], update);
+		res.json(updatedProduct);
+	}
+});
+app.delete('/api/getProduct/:id', function(req, res) {
+	var productIndex = _.findIndex(products, {_id: req.params.id});
+	if (!products[productIndex]) {
+		res.send('not found');
+	} else {
+		var deletedProduct = products[productIndex];
+}
+});
+
+/*testing github*/
+app.listen(port, function () {
+	console.log('Listening on port', port);
 });
